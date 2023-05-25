53210b972346de6464524cc831cc1687f5af59ee
Vitalii MV <agronom9913@gmail.com>
2023-05-25 19:44:14 +0300

    2023.05.25

--- src/App.js
+++ src/App.js
@@ -3,32 +3,31 @@ import "./App.css";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-
 import "./scss/app.scss";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import Cart from './pages/Cart'
+import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 
-// import pizzas from "./assets/pizzas.json";
+export const SearchContext = React.createContext();
 
 function App() {
-  const [searchValue, setSearchValue] = React.useState('');
+  const [searchValue, setSearchValue] = React.useState("");
+
   return (
     <div class Name="wrapper">
-      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
-      <div className="content">
-        <div className="container">
-          {/* <NotFound /> */}
-          {/* <Home /> */}
-          <Routes>
-            <Route path="/" element={<Home searchValue={searchValue} />} />
-            <Route path="/cart" element={<Cart />} />
-
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+      <SearchContext.Provider value={{searchValue, setSearchValue}}>
+        <Header />
+        <div className="content">
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </SearchContext.Provider>
     </div>
   );
 }
