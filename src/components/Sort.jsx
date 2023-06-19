d269ad194a6bf20cb916ec689023b47905a9fb44
Vitalii MV <agronom9913@gmail.com>
2023-06-19 18:23:06 +0300

    2023.06.19 18-22

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -15,25 +15,17 @@ const Sort = () => {
   const dispatch = useDispatch();
   const sort = useSelector((state) => state.filter.sort);
   const sortRef = React.useRef();
-
   const [open, setOpen] = React.useState(false);
 
   const onClickListItem = (obj) => {
     dispatch(setSort(obj));
     setOpen(false);
   };
-  // React.useEffect(() => {
-  //   const handleClickOutside = (event) => {
-  //     if (!event.path.includes(sortRef.current)) {
-  //       setOpen(false);
-  //     }
-  //   };
 
-  //   document.body.addEventListener("click", handleClickOutside);
-  //   return () => {
-  //     document.body.removeEventListener('click',handleClickOutside)
-  //   };
+  // React.useEffect(() => {
+ 
   // }, []);
+
 
   return (
     <div ref={sortRef} className="sort">
